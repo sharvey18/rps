d23913f76a547e1192ba9f7f769007229057c2d7
Sam Harvey <s.rharveyiv@gmail.com>
2017-09-30 21:08:27 -0700

    complete
    rock, paper, scissors game completed

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Rock Paper Sissors</title>
+    <link rel="stylesheet" href="/sass/style.min.css">
+  </head>
+  <body>
+      <div class="title">Rock Paper Scissors</div>
+    <div class="container">
+      <h1>Choose Your Weapon</h1>
+      <img src="images/rock.svg" alt="rock">
+      <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
+      <img src="images/paper.svg" alt="paper">
+      <!-- <div>Icons made by <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
+      <img src="images/scissors.svg" alt="scissors">
+      <!-- <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div> -->
+      <p></p>
+    </div>
+    <script src="/js/app.js" charset="utf-8"></script>
+  </body>
+</html>
